df4ed5acd1b0f938261537143a5cbe6e570b4ff0
asix2pj9 <asix2pj9@proton.me>
2025-02-10 07:17:47 +0000

    Commit 5.4 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -10,25 +10,23 @@
           if (empty($_GET['ingr'])) {
                $pvp=((100+$IVA)/100)*$preu;
 	       echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-   	       exit(0);
+   	       # exit(0);
           }
           else {
                $IVA=21;
 	       $pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
 	       echo "Has demanat la pizza bàsica amb:<br> ";
-	      foreach ($_GET['ingr'] as $ingredient) {
-	           if ($ingredient) {
-		        echo "* $ingredient<br>";
-		   }
-	      }
-	      echo "El preu és $pvp € (IVA inclòs)<br>";
-	      exit(0);
+	       foreach ($_GET['ingr'] as $ingredient) {
+	            if ($ingredient) {
+		         echo "* $ingredient<br>";
+		    }
+	       }
+	       echo "El preu és $pvp € (IVA inclòs)<br>";
+	       # exit(0);
           }
 	  echo "<br>";
 	  echo "Autor aplicatiu: dacomo"."<br>";
      ?>
-     <button onclick="location.href='pizza.html'" type="button">
-          Retorna al formulari
-     </button>
+     <a href="pizza.html">Retorna al formulari</a>
      </body>
 </html>
