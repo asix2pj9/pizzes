542456510b334c622b909699fe1f56854713b13e
asix2pj9 <asix2pj9@proton.me>
2026-04-13 11:07:25 +0200

    Update pizza.php - 20260413 - 11.07.25

--- codi/pizza.php
+++ codi/pizza.php
@@ -12,8 +12,7 @@
 	    		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
 			}          
           	else {
-            	$IVA=21;
-	    		$pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
+            	$pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
 	     		echo "Has demanat la pizza bàsica amb:<br> ";
 	     		foreach ($_GET['ingr'] as $ingredient) {
 	          		if ($ingredient) {
